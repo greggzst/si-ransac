85ad0b0b605f85b6454308f6de0b2d7afec84407
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-06-08 10:44:08 +0200

    add Point class

---
+++ RANSAC/Structures/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RANSAC.Structures
+{
+    class Point
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public Point(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
